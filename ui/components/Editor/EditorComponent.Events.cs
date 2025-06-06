f2e2b7e088fb6dd518bd563e91017141a6bf41f9
MilkyCoding <milkycoding666@gmail.com>
2025-06-06 21:04:57 +0300

    Add Ctrl+S hotkey support for saving files (Save/Save As) in the editor

--- ui/components/Editor/EditorComponent.Events.cs
+++ ui/components/Editor/EditorComponent.Events.cs
@@ -24,11 +24,27 @@ namespace NimblyApp
         {
             if (e.KeyCode == Keys.Tab && !e.Control && !e.Alt && !e.Shift)
             {
+                e.Handled = true;
                 // Отменяем стандартное поведение Tab
                 e.SuppressKeyPress = true;
 
                 // Вставляем 4 пробелов вместо символа табуляции
                 _textBox.SelectedText = "    "; // 4 пробелов
+            }
+
+            // Сохранение файла
+            if (e.Control && e.KeyCode == Keys.S)
+            {
+                if (string.IsNullOrEmpty(_currentFilePath))
+                {
+                    SaveFileAs();
+                }
+                else
+                {
+                    SaveFile();
+                }
+                e.Handled = true;
+                e.SuppressKeyPress = true;
             }
         }
     }
